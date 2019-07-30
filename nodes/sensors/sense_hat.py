49a4aeb55dbad68ae9d014daeb1ec1ca684176ae
Nicolas Baudet <nico.baudet@gmail.com>
2019-07-30 16:15:28 +0200

    Update round method

--- nodes/sensors/sense_hat.py
+++ nodes/sensors/sense_hat.py
@@ -1,4 +1,5 @@
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
+import json
 from .base_sensor import BaseSensor
 from sense_hat import SenseHat
 
@@ -6,7 +7,7 @@ from sense_hat import SenseHat
 class SenseHAT(BaseSensor):
     """
     Used to read data from the Sense HAT board's sensors
-    
+
     Please read following documentation to install and activate
     necessary kernel modules on your Raspberry Pi.
     Documentation:
@@ -27,22 +28,29 @@ class SenseHAT(BaseSensor):
     name = 'SenseHAT'
 
     def round(self, float_number):
-        return Decimal(float_number).quantize(Decimal('.1'), rounding=ROUND_HALF_EVEN)
+        return Decimal(float_number).quantize(Decimal('1.0'), rounding=ROUND_HALF_UP)
 
     def _read_data(self):
         sense = SenseHat()
-        sense.show_message('Hello world!')
+        # sense.show_message('Hello world!')
 
         # Float, the percentage of relative humidity
-        humid = round(sense.get_humidity())
+        humid = self.round(sense.get_humidity())
 
         # Float, the current temperature in degrees Celsius
-        temp = round(sense.get_temperature_from_humidity())
+        temp_h = self.round(sense.get_temperature_from_humidity())
 
         # Other way to get the temperature from the Sense HAT
-        # temp = round(sense.get_temperature_from_pressure())
+        temp_p = self.round(sense.get_temperature_from_pressure())
 
         # Float, the current pressure in Millibars
-        press = round(sense.get_pressure())
+        press = self.round(sense.get_pressure())
 
-        return f'{"temp": {temp}, "humid": {humid}, "press": {press}}'
+        data = {
+            "temp_humidity": str(temp_h),
+            "temp_pressure": str(temp_p),
+            "humid": str(humid),
+            "press": str(press)
+        }
+
+        return json.dumps(data)
